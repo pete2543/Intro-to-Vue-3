059b378f2051548c76cf464e2f667af8bbffc3c2
pete2543 <petekaza2543@gmail.com>
2021-03-24 11:08:59 +0700

    s

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: 8,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
